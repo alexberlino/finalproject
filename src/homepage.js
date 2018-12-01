f6bbb476332f966679be2fbb7c33515fd2c2cbc2
Alex Bieth <alex.bieth@gmail.com>
2018-12-01 12:07:27 +0100

    HP improvements audit txt

--- src/homepage.js
+++ src/homepage.js
@@ -79,30 +79,58 @@ class Homepage extends Component {
                     <span>Off-Page Audit</span>
                 </div>
 
-                <div className="intro">
-                    <footer className="txt2 txthead">
-                        {" "}
-                        <img alt="about me" src="/me.svg" className="icons" />
-                        {t("aboutmetitle") + ">"}
-                    </footer>
-
+                <footer className="txt2 txthead">
+                    <img alt="about me" src="/me.svg" className="icons" />
+                    {t("aboutmetitle") + "> "}{" "}
                     <h1 className="txt">{t("intro1")}</h1>
-                    <div className="txt">{t("intro2")}</div>
-                    <div className="txt">{t("intro4")}</div>
-                    <div className="txt">{t("intro3")}</div>
+                </footer>
 
-                    <div className="txt">
-                        <Link
-                            to={"/" + this.props.i18n.language + "/about"}
-                            onClick={() => this.props.pageChange("/about")}
-                        >
-                            <img
-                                alt="more"
-                                src="/more.svg"
-                                className="iconsmore"
-                                title="read more"
-                            />
-                        </Link>
+                <div className="intro">
+                    <div className="">
+                        <p className="txt2 block">{t("intro2")}</p>
+                        <p className="txt2 block">{t("intro4")}</p>
+                        <p className="txt2 block">{t("intro3")}</p>
+
+                        <div className="txt">
+                            <Link
+                                to={"/" + this.props.i18n.language + "/about"}
+                                onClick={() => this.props.pageChange("/about")}
+                            >
+                                <img
+                                    alt="more"
+                                    src="/more.svg"
+                                    className="iconsmore"
+                                    title="read more"
+                                />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="">
+                        <img
+                            alt="monitoring"
+                            src="/seo-monitoring.jpg"
+                            className="icon"
+                        />
+
+                        <p className="txt2 block"> SEO</p>
+                        <p className="txt2 block">Analytics </p>
+                        <p className="txt2 block">{t("intro5")}</p>
+
+                        <p className="txt2 block"> SEA</p>
+                    </div>
+                    <div className="">
+                        <img
+                            alt="programming"
+                            src="/programming.svg"
+                            className="icon"
+                        />
+
+                        <p className="txt2 block"> React, Vue, Handlebars</p>
+                        <p className="txt2 block">Javascript, JQuery </p>
+                        <p className="txt2 block">Node, Express</p>
+
+                        <p className="txt2 block"> HTML, CSS</p>
+                        <p className="txt2 block"> SQL, Postgres</p>
                     </div>
                 </div>
                 {this.state.show ? (
@@ -131,99 +159,79 @@ class Homepage extends Component {
                     </div>
                 ) : null}
 
-                <div className="intro2">
-                    <div className="introQuarter">
-                        <img
-                            alt="monitoring"
-                            src="/seo-monitoring.jpg"
-                            className="icon"
-                        />
-
-                        <p className="txt2 block"> SEO</p>
-                        <p className="txt2 block">Analytics </p>
-                        <p className="txt2 block">{t("intro5")}</p>
-
-                        <p className="txt2 block"> SEA</p>
-                    </div>
-                    <div className="introQuarter">
-                        <img
-                            alt="programming"
-                            src="/programming.svg"
-                            className="icon"
-                        />
-
-                        <p className="txt2 block"> React, Vue, Handlebars</p>
-                        <p className="txt2 block">Javascript, JQuery </p>
-                        <p className="txt2 block">Node, Express</p>
-
-                        <p className="txt2 block"> HTML, CSS</p>
-                        <p className="txt2 block"> SQL, Postgres</p>
-                    </div>
-                </div>
+                <div className="intro2" />
+                <footer className="txt2 txthead">audit content></footer>
 
                 <div className="introQuarter2">
-                    <footer className="txt2 txthead">audit content></footer>
-                    <span className="block bold">Executive Summary </span>
-                    <span className="block bold">Audit Explanation</span>
-                    <span className="block bold">1. Keyword Research</span>
-                    <span className="block">
-                        keyword brainstorm using current traffic/ Paid search /
-                        Tools
-                    </span>
-                    <span className="block">
-                        Current Analysis including competitors rankings
-                    </span>
-                    <span className="block">Suggestions</span>{" "}
-                    <span className="block bold">2. On-page Audit</span>
-                    <span className="block">
-                        content for traffic, looking optimzation of site vs
-                        Keyword Strategy
-                    </span>
-                    <span className="block">
-                        internal linking & site structure
-                    </span>
-                    <span className="block">meta tags and headers</span>
-                    <span className="block">images</span>
-                    <span className="block">structured data</span>
-                    <span className="block">
-                        duplicate content and canonicals
-                    </span>
-                    <span className="block bold">3. Technical SEO</span>
-                    <span className="block">indexation</span>
-                    <span className="block">crawlability</span>
-                    <span className="block">
-                        page speed & mobile-friendliness
-                    </span>
-                    <span className="block bold">
-                        4. Off-page: Backlink Profile
-                    </span>
-                    <span className="block">overview</span>
-                    <span className="block">trend</span>
-                    <span className="block">best types of links</span>
-                    <span className="block">brandbuilding suggestions</span>
-                    <span className="block bold">5. Competitor Analysis:</span>
-                    <span className="block">backlink & content</span>
-                    <span className="block bold">
-                        6. Summary Priority To-Do List
-                    </span>
-                    <span className="block">low hanging fruit</span>
-                    <span className="block">projects</span>
-                    <p className=" italic introQuarter ">
-                        documents and access needed before audit: Search Console
-                        & Google Analytics, Business Overview, Industry Market
-                        research including competition, geographical & personas
-                        targets, Google Ads data
-                    </p>
-                    <span className="block bold">
-                        pdf report layout example
-                    </span>
-                    <div>
-                        {" "}
+                    <div className="footerQuarter">
+                        <span className="block bold">Executive Summary </span>
+                        <span className="block bold">1. Keyword Research</span>
+                        <span className="block">
+                            keyword brainstorm from current traffic/ ppc / many
+                            tools
+                        </span>
+                        <span className="block">
+                            current main KWs rankings including competitors
+                        </span>
+                        <span className="block bold">2. On-page Audit</span>
+                        <span className="block">
+                            content for traffic, site vs optimum keyword
+                            strategy
+                        </span>
+                        <span className="block">
+                            internal linking & site structure
+                        </span>
+                        <span className="block">
+                            meta tags & headers, images, SD
+                        </span>
+                        <span className="block">
+                            duplicate content and canonicals
+                        </span>
+                    </div>
+
+                    <div className="footerQuarter">
+                        <span className="block bold">3. Technical SEO</span>
+                        <span className="block">indexation, crawlability</span>
+                        <span className="block">
+                            page speed & mobile-friendliness
+                        </span>
+                        <span className="block bold">
+                            4. Off-page: Backlink Profile
+                        </span>
+                        <span className="block">overview</span>
+                        <span className="block">trend</span>
+                        <span className="block">best types of links</span>
+                        <span className="block">brandbuilding suggestions</span>
+                        <span className="block bold">
+                            5. Competitor Analysis:
+                        </span>
+                        <span className="block">backlink & content</span>
+                    </div>
+                    <div className="footerQuarter">
+                        <span className="block bold">
+                            6. Summary Priority To-Do List
+                        </span>
+                        <span className="block">low hanging fruit</span>
+                        <span className="block">projects</span>
+                        <p className=" italic introQuarter ">
+                            documents and access needed before audit: Search
+                            Console & Google Analytics, Business Overview,
+                            Industry Market research including competition,
+                            geographical & personas targets, Google Ads data
+                        </p>
+                    </div>
+
+                    <div className="footerQuarter">
+                        <span className="block bold">
+                            report layout example
+                        </span>{" "}
                         <a href="/reporttemplate.pdf">
                             <img
                                 src="/auditexample.png"
                                 height="200px"
                                 width="130px"
+                                alt="seo audit template"
+                                title="audit outline example, no content (original 25 pages long) for confidentiality reasons"
                             />{" "}
                         </a>
                     </div>
