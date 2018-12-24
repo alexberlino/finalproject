d1b6326fb1d683d21248037b331b00e337362ff3
Alex Bieth <alex.bieth@gmail.com>
2018-12-24 10:27:15 +0100

    backlink audit page content

--- src/Offpage/BacklinkAnalysis.js
+++ src/Offpage/BacklinkAnalysis.js
@@ -15,7 +15,7 @@ class BacklinkAnalysis extends Component {
       <div className="mainRightServices">
         <Helmet>
           <meta charSet="utf-8" />
-          <title>Offpage SEO - Backlink Analysis | SEO Berlino</title>
+          <title>Offpage SEO - Backlink Profile Audit | SEO Berlino</title>
           <meta type="description" content="" />
           <link
             rel="canonical"
@@ -31,46 +31,130 @@ class BacklinkAnalysis extends Component {
             <a href={"/" + this.props.lng + "/offpage"}> OFFPAGE </a>
           </div>
           <div className="mainRightServices">
-            <h1 className="h1services"> Backlink Analysis</h1>
+            <h1 className="h1services"> Backlink Profile Audit</h1>
             <div className="serviceText">
-              <li>Analyse your backlink profile and of your competitors.</li>
-              <li>
-                Create a report with your profile's pros and cons, including
-                *toxic links*. Follow and no-follow, quality of links, anchor
-                text.
-              </li>
-              <li>
-                Based on your industry and your competition, make some
-                recommendations with an action plan, focusing on *brand
-                building*.
-              </li>
+              <p>
+                Create a report with your profile's pros and cons - vs target
+                and taking into account competitors - including{" "}
+                <a
+                  onClick={() => this.props.pageChange("/offpage/toxic")}
+                  href="/en/offpage/toxic"
+                >
+                  *toxic links*
+                </a>
+                quantity and quality links and set some objectives for the next
+                quarters focusing on *brand building*
+              </p>
             </div>
-            <div className="serviceTitle">
-              {" "}
-              Backlinks are still very important in 2019
-            </div>
+            <img
+              src="/offpage.svg"
+              className="bigicon"
+              heigth="200px"
+              width="200px"
+            />
+            <h2 className="serviceTitle">
+              Fact: Backlinks are still very important in 2019
+            </h2>
             <div className="serviceText">
-              It is crucial to understand where you stand, plan, execute and
-              control your backlinks activities.
+              Like it was the case 20 years ago, backlinks are key to SEO
+              success and still define the reputation and popularity of your
+              brand on the web. However, backlinks do not work like 20 years
+              ago, and far from it. It is now much more about quality of the
+              links and relevancy. It is crucial to understand where you stand,
+              plan, execute and control your backlink activities.
             </div>
             <h2 className="serviceTitle">Quality over quantity</h2>
-            <h2 className="serviceTitle">
-              Analyse your site's profile but don't forget to check your
-              competitors
-            </h2>
-            <h2 className="serviceTitle">Anchor text</h2>
+            <img
+              src="/qualityoverquantity.jpeg"
+              className="bigicon"
+              heigth="200px"
+              width="200px"
+            />
+            <div className="serviceText">
+              Getting many backlinks from any website, paying links off from
+              unrelated websites, or websites operating from a different country
+              or with a different language are some example of wasted efforts.
+              Most importantly, if the website which is linking to you has a
+              poor reputation itself its value will be only minimum; worse if it
+              is considered a spammy website, the link might even lower your
+              website's reputation.
+            </div>
             <h2 className="serviceTitle">
               Refering Domains: page authority over domain authority
             </h2>
+            <div className="serviceText">
+              Getting a link from a reputable and relevant (to your industry)
+              website is a great achievement. However keep in mind the actual
+              page the link is appearing on is the real deal. If the page itself
+              has little exposure, little number of internal links and/or
+              traffic, its impact will not be as positive as if it was on the
+              homepage for example. On the contrary, if it appears in every page
+              of the site, this might seen as spammy backlinking so avoid that
+              extreme as well.
+            </div>
+            <h2 className="serviceTitle">
+              Compare your backlink profile to those of your main competitors -
+              to keep a benchmark in mind.
+            </h2>
+            <div className="serviceText">
+              It is very important and useful to look at your successful
+              competitors to analyse:
+              <ul>
+                <li>how they get their (high quality) links</li>
+                <li>
+                  the domain authority and quality of their backlink profile
+                </li>
+                <li>
+                  ratio follow/nofollow, anchor text, ratio quality links,
+                  number & quality of referring domains
+                </li>
+              </ul>
+            </div>
+            <h2 className="serviceTitle">
+              Anchor text, nofollow/follow, gov&edu, etc
+            </h2>
+            <div className="serviceText">
+              Some other of the important factors to look into are:
+              <ul>
+                <li>
+                  anchor text: make sure they look natural, avoid "click here"
+                  or some misleading text. Very often the brand name is the most
+                  used anchor text.
+                </li>
+                <li>
+                  make sure the ratio of nofollow links is not too high.
+                  nofollow means that the website linking does not want to
+                  "commit" the link as sign a trust and therefore pass link
+                  juice (authority). It is totally acceptable to have a certain
+                  percentage of nofollow links but if this ration gets to high,
+                  this is sign that your website is not trusted.{" "}
+                </li>
+                <li>
+                  ".gov" and ".edu" sites have by essence a lot more trust and
+                  power as ".com" for instance. If you have the opportunity to
+                  get alink from such sites and it makes sense to your business
+                  - bringing in at the same time quality & relevant traffic -
+                  this link will very likely be beneficial.
+                </li>
+              </ul>
+            </div>
             <h2 className="serviceTitle"> Related Tools and Resources</h2>
             <div className="serviceText">
-              <a className="block" href="https://moz.com/link-explorer">
+              <a
+                target="blank"
+                className="block"
+                href="https://moz.com/link-explorer"
+              >
                 Moz Link Explorer
               </a>{" "}
-              <a className="block" href="https://de.majestic.com/">
+              <a
+                target="blank"
+                className="block"
+                href="https://de.majestic.com/"
+              >
                 Majestic SEO
               </a>
-              <a className="block" href="https://ahrefs.com/de/">
+              <a target="blank" className="block" href="https://ahrefs.com/de/">
                 ahrefs
               </a>{" "}
             </div>{" "}
