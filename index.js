6af951a8c3002329310a4aeb114e5fdca7375fe4
Alex Bieth <alex.bieth@gmail.com>
2019-02-05 13:42:07 +0100

    redirect https

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ var server = http.createServer((req, res) => {
     res.end();
 });
 
+app.use(function(req, res, next) {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect("https://" + req.headers.host + req.url);
+    }
+});
+
 app.engine(
     ".hbs",
     hb({
