28ea969da6ce300163c5b226611f439362ba1672
Alex Bieth <alex.bieth@gmail.com>
2019-02-05 12:44:29 +0100

    redirect https

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
+app.use(function(req, res, next) {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect("https://" + req.headers.host + req.url);
+    }
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
