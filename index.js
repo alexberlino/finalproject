6cfeb144a9edbe974c462706a5e4f1a083584d1d
Alex Bieth <alex.bieth@gmail.com>
2019-02-05 13:35:43 +0100

    redirect https

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ var server = http.createServer((req, res) => {
     res.end();
 });
 
+var http = express.createServer();
+
+// set up a route to redirect http to https
+http.get("*", function(req, res) {
+    res.redirect("https://" + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+});
+
 app.engine(
     ".hbs",
     hb({
