54766c99080e54868f0420254f62fbbf205dbe6f
Alex Bieth <alex.bieth@gmail.com>
2020-03-03 20:09:59 +0100

    captchas2 fix

--- index.js
+++ index.js
@@ -1894,8 +1894,7 @@ app.post('/check', async (req, res) => {
     }
 })
 
-app.post('/email', (req, res) => {
-
+app.post('/check', async (req, res) => {
 
     nodemailer.createTestAccount((error, account) => {
         const htmlEmail = `
