4c8d06a5ed01b69946c98b126d48a2e38f153f27
Alex Bieth <alex.bieth@gmail.com>
2020-08-03 15:35:20 +0200

    remove js

--- Public/js/general-min.js
+++ Public/js/general-min.js
@@ -1,19 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const e = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-    e.length > 0 && e.forEach(e => {
-        e.addEventListener("click", () => {
-            const t = e.dataset.target,
-                l = document.getElementById(t);
-            e.classList.toggle("is-active"), l.classList.toggle("is-active"), document.body.classList.toggle("frozen")
-        })
-    })
-});
-const panels = document.querySelectorAll(".dropdown-arrow");
+// Navbar Toggle
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+if ($navbarBurgers.length > 0) {
+
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+            document.body.classList.toggle("frozen");
+        });
+    });
+}
+
+
+
+// Mobile Menu Dropdown Activation
+const panels = document.querySelectorAll('.dropdown-arrow');
 
 function toggleOpen() {
-    console.log("Hello"), this.classList.toggle("activate"), this.nextElementSibling.nextElementSibling.classList.toggle("activate"), this.parentElement.classList.toggle("activate")
+    this.classList.toggle('activate');
+    this.nextElementSibling.nextElementSibling.classList.toggle('activate');
+    this.parentElement.classList.toggle('activate');
 }
-panels.forEach(e => e.addEventListener("click", toggleOpen)), window.sr = ScrollReveal(), sr.reveal(".hero-body, .header-body", {
-    duration: 0,
-    interval: 0
-});
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
