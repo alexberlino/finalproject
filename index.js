5e77d8cfc12bf398c22a17acf904c3be681bfe24
Alex Bieth <alex.bieth@gmail.com>
2020-08-14 22:03:47 +0200

    www issue revert

--- index.js
+++ index.js
@@ -48,9 +48,12 @@ i18n.configure({
 app.use(i18n.init);
 
 if (process.env.NODE_ENV === "production") {
-    app.get("*", function(req, res, next) {
-        if (req.headers.host.slice(0, 3) != "www") {
-            res.redirect("http://www." + req.headers.host + req.url, 301);
+    app.use(function(req, res, next) {
+        if (req.headers["x-forwarded-proto"] !== "https") {
+            return res.redirect(
+                301,
+                ["https://www.seoberlino.com", req.url].join("")
+            );
         } else {
             next();
         }
@@ -58,12 +61,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 if (process.env.NODE_ENV === "production") {
-    app.use(function(req, res, next) {
-        if (req.headers["x-forwarded-proto"] !== "https") {
-            return res.redirect(
-                301,
-                ["https://www.seoberlino.com", req.url].join("")
-            );
+    app.get("*", function(req, res, next) {
+        if (req.headers.host.slice(0, 3) != "www") {
+            res.redirect("http://www." + req.headers.host + req.url, 301);
         } else {
             next();
         }
