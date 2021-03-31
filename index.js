8f58fe5cddf31b85adca068e1f19f8593a9a826c
Alex Bieth <alex.bieth@gmail.com>
2021-03-31 13:07:02 +0200

    berlin in HP

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ app.get("/de", (req, res) => {
     res.render("home-min", {
         requrl: localhost + "/en",
         layout: "mainDEHP-min",
-        title: "SEO Beratung • SEO Agentur | SEO Berlino",
+        title: "SEO Beratung • SEO Agentur Berlin | SEO Berlino",
         canonical: localhost + "/de",
         description: "SEO Beratung in Berlin mit SEO Berlino, Kleine SEO Firma • SEO Berater mit 10 Jahre Erfahrung, Suchmaschinenoptimierung Agentur in Berlin. Kunden: Montblanc, HelloFresh, Ricoh, Spreadshirt, Spartoo, BSH etc",
         alt: localhost + "/en",
